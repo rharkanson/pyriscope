f75f5d97163609135c06cc532aa1596814bb2d82
Russell Harkanson <rharkanson@gmail.com>
2015-10-19 10:02:45 -0700

    Initial commit

--- pyriscope/__init__.py
+++ pyriscope/__init__.py
@@ -1,1 +1,14 @@
 __author__ = 'Russell Harkanson'
+
+import sys
+
+if __name__ == '__main__':
+    import pyriscope.pyriscope
+
+    sys.argv.pop(0)
+    if len(sys.argv) == 1 and sys.argv[0] == "__magic__":
+        pyriscope.main(input("Enter args now: ").strip(' ').split(' '))
+    else:
+        pyriscope.main(sys.argv)
+else:
+    from pyriscope import pyriscope
